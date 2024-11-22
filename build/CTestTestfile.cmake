115eda6f84a5819cf066124a739c72e1b4e3ca57
caiogsmendes <caio.g.ds.mendes@gmail.com>
2024-11-22 14:39:07 -0300

    hwtest04_2

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hetech/para deletar/gnss-sdr
-# Build directory: /home/hetech/para deletar/gnss-sdr/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/algorithms/acquisition/gnuradio_blocks/CMakeFiles/acquisition_gr_blocks.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for acquisition_gr_blocks.
-# This may be replaced when dependencies are built.
