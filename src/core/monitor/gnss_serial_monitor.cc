17c8e0f8d0f21a5d62454450638d4b7a7a9f8aa1
caiogsmendes <caio.g.ds.mendes@gmail.com>
2024-11-25 09:40:00 -0300

    hwtest06_1

--- src/core/monitor/gnss_serial_monitor.cc
+++ src/core/monitor/gnss_serial_monitor.cc
@@ -91,90 +91,95 @@ int gnss_serial_monitor::general_work(int noutput_items __attribute__((unused)),
     // // const auto** in1 = reinterpret_cast<const Gps_Ephemeris**>(&input_items[0]);
 
 
-    typedef struct gpiod_line gpiod_pin;
-    typedef struct gpiod_line_event gpiod_pin_event;
-    struct gpiod_chip *chip;
-    gpiod_pin *pin;
-    const char bank[] = "gpiochip2";
-    int SODIMM_55 = 18;
-    // int SODIMM_63;
-    unsigned int line = SODIMM_55;
+    // typedef struct gpiod_line gpiod_pin;
+    // typedef struct gpiod_line_event gpiod_pin_event;
+    // struct gpiod_chip *chip;
+    // gpiod_pin *pin;
+    // const char bank[] = "gpiochip2";
+    // int SODIMM_55 = 18;
+    // // int SODIMM_63;
+    // unsigned int line = SODIMM_55;
 
-    timespec ts;
-    ts.tv_nsec=100000000000;
+    // timespec ts;
+    // ts.tv_nsec=100000000000;
 
-    chip = gpiod_chip_open_by_name(&bank[0]);
-    pin = gpiod_chip_get_line(chip, line);
-    // gpiod_pin *input_pin;
-    gpiod_pin_event event;
-    // int pin_value = 0;
-    int ret;
-    ret = gpiod_line_request_rising_edge_events(pin, "gpio-test");
-    int count = 0;
-    while (1)
-        {
-            // mtx.lock();
-            // // gnss_synchro = pvt_ptr->get_gnss_observables();
-            // // uint8_t *msgvec_ptr = pvt_ptr->get_msgvec_ptr();
-            // do
-            //     {
-            //     }
-            // while (counter <= bytess);
-            // mtx.unlock();
+    // chip = gpiod_chip_open_by_name(&bank[0]);
+    // pin = gpiod_chip_get_line(chip, line);
+    // // gpiod_pin *input_pin;
+    // gpiod_pin_event event;
+    // // int pin_value = 0;
+    // int ret;
+    // ret = gpiod_line_request_rising_edge_events(pin, "gpio-test");
+    // int count = 0;
+    // // while (1)
+    // //     {
+    //         // mtx.lock();
+    //         // // gnss_synchro = pvt_ptr->get_gnss_observables();
+    //         // // uint8_t *msgvec_ptr = pvt_ptr->get_msgvec_ptr();
+    //         // do
+    //         //     {
+    //         //     }
+    //         // while (counter <= bytess);
+    //         // mtx.unlock();
 
-            /* Waiting for an event on the input pin */
-            gpiod_line_event_wait(pin, NULL);
-            // else
-            // {
-            //     std::cout<<"Trigged"<<"\n";
-            // }
-            // gpiod_line_event_wait(pin, &ts);
+    //         /* Waiting for an event on the input pin */
+    //         gpiod_line_event_wait(pin, NULL);
+    //         // else
+    //         // {
+    //         //     std::cout<<"Trigged"<<"\n";
+    //         // }
+    //         // gpiod_line_event_wait(pin, &ts);
 
-            /* Reading next pending event from the GPIO pin */
-            if (gpiod_line_event_read(pin, &event) != 0)
-                continue;
+    //         /* Reading next pending event from the GPIO pin */
+    //         if (gpiod_line_event_read(pin, &event) != 0)
+    //             {
+    //                 std::cout<<"Error"<<"\n";
+    //             }
 
-            /* Checking if it is a rising event as previously defined */
-            if (event.event_type != GPIOD_LINE_EVENT_RISING_EDGE)
-                continue;
+    //         /* Checking if it is a rising event as previously defined */
+    //         if (event.event_type != GPIOD_LINE_EVENT_RISING_EDGE)
+    //             {
+                    
+    //             }
             
-            std::cout << "Trigged" << "\n";
-            // else{
-            //     std::cout<<""<<"\n";
-            // }
-            // continue;
+    //         std::cout << "Trigged" << "\n";
+    //         // else{
+    //         //     std::cout<<""<<"\n";
+    //         // }
+    //         // continue;
 
-            // std::cout << "Detected " << count++;
-            // // int result = write(,&pvt_ptr->msgvec[0],bytes);
-            // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    //         // std::cout << "Detected " << count++;
+    //         // // int result = write(,&pvt_ptr->msgvec[0],bytes);
+    //         // std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-            // }
+    //         // }
 
 
-            // // Loop through each input stream channel
-            // for (int channel_index = 0; channel_index < d_nchannels; channel_index++)
-            //     {
-            //         // Loop through each item in each input stream channel
-            //         for (int item_index = 0; item_index < ninput_items[channel_index]; item_index++)
-            //             {
-            //                 // Use the count variable to limit how many items are sent per channel
-            //                 count++;
-            //                 if (count >= 10)
-            //                     {
-            //                         // Convert to a vector and write to the UDP sink
-            //                         std::vector<Gnss_Synchro> stocks;
-            //                         stocks.push_back(in[channel_index][item_index]);
-            //                         // udp_sink_ptr->write_gnss_synchro(stocks);
+            // Loop through each input stream channel
+            for (int channel_index = 0; channel_index < d_nchannels; channel_index++)
+                {
+                    // Loop through each item in each input stream channel
+                    for (int item_index = 0; item_index < ninput_items[channel_index]; item_index++)
+                        {
+                            // Use the count variable to limit how many items are sent per channel
+                            count++;
+                            if (count >= 10)
+                                {
+                                    // Convert to a vector and write to the UDP sink
+                                    std::vector<Gnss_Synchro> stocks;
+                                    stocks.push_back(in[channel_index][item_index]);
+                                    // udp_sink_ptr->write_gnss_synchro(stocks);
 
-            //                         // Reset count variable
-            //                         // count = 0;
+                                    // Reset count variable
+                                    // count = 0;
 
-            //                         // Consume the number of items for the input stream channel
-            //                         consume(channel_index, ninput_items[channel_index]);
-            //                     }
-            //             }
+                                    // Consume the number of items for the input stream channel
+                                    consume(channel_index, ninput_items[channel_index]);
+                                }
+                        }
         }
     // // Not producing any outputs
+
     return 0;
 }
 
