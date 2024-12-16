94b1298be8f8e33fc81c57261857f424a8d62bf5
caiogsmendes <caio.g.ds.mendes@gmail.com>
2024-12-16 11:57:51 -0300

    HW4test09_3

--- src/core/receiver/gnss_flowgraph.cc
+++ src/core/receiver/gnss_flowgraph.cc
@@ -113,7 +113,7 @@ void GNSSFlowgraph::init()
     //Caio - Serial
     std::string serial_default = "/dev/ttyLP2";
     std::string serial_device = configuration_->property("GNSS-SDR.serial_device",serial_default);
-    comms_flow = HEserial_connect(serial_device.c_str(), B921600, O_RDWR | O_NDELAY | O_NOCTTY | O_NONBLOCK);
+    comms_flow = HEserial_connect(serial_device.c_str(), B921600, O_RDWR | O_NDELAY | O_NOCTTY);// | O_NONBLOCK);
     //
 
     channels_status_ = channel_status_msg_receiver_make();
